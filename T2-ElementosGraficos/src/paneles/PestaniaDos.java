fba38e563dc0cf06d17558e4e053a4c2fbeab924
Adri <adrian.olmo@cesjuanpablosegundo.es>
2019-10-23 11:09:55 +0200

    Proyectos DI

---
+++ T2-ElementosGraficos/src/paneles/PestaniaDos.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paneles;
+
+import javax.swing.*;
+
+public class PestaniaDos extends JPanel {
+
+    public PestaniaDos(){
+        initGUI();
+    }
+
+    private void initGUI() {
+
+    }
+}
+
--- T2-ElementosGraficos/src/t2/elementos/Entrada.java
+++ T2-ElementosGraficos/src/t2/elementos/Entrada.java
@@ -5,17 +5,21 @@
  */
 package t2.elementos;
 
+import javax.swing.SwingUtilities;
+
 /**
  *
  * @author Usuario DAM 2
  */
 public class Entrada {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Ventana v = new Ventana();
+                v.initGUI();
+            }
+        });
     }
-    
 }
