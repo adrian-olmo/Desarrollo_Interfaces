9ce56979a1214c63971ac11208a06d9e4f585b50
Adri <adrian.olmo@cesjuanpablosegundo.es>
2019-10-23 11:18:22 +0200

    Proyectos DI

---
+++ T2 - ElementosGraficos/Botones/src/entrada/Ventana.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entrada;
+
+import javax.swing.*;
+import java.awt.*;
+import paneles.PestaniaDos;
+import paneles.PestaniaUno;
+
+public class Ventana extends JFrame {
+
+    Container container;
+    // panel personalizado
+    PestaniaUno pestaniaUno;
+    PestaniaDos pestaniaDos;
+    JTabbedPane panelPestanias;
+
+    public void initGUI(){
+
+        instancias();
+        configurarContainer();
+        this.setSize(new Dimension(300,300));
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        //this.pack();
+    }
+
+    private void configurarContainer() {
+        container.add(panelPestanias);
+        panelPestanias.addTab("Botones",pestaniaUno);
+        panelPestanias.addTab("Check y radios",pestaniaDos);
+    }
+
+    private void instancias() {
+        container = this.getContentPane();
+        panelPestanias = new JTabbedPane(SwingConstants.TOP);
+        pestaniaUno = new PestaniaUno();
+        pestaniaDos = new PestaniaDos();
+    }
+}
+
