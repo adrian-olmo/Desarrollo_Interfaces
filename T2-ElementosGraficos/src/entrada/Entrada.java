7ce3d62ef41903e1e9a5126a04ef9e317bd36214
Adri <adrian.olmo@cesjuanpablosegundo.es>
2019-10-23 11:10:45 +0200

    Proyectos DI

--- T2-ElementosGraficos/src/entrada/Entrada.java
+++ T2-ElementosGraficos/src/entrada/Entrada.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package t2.elementos;
+package entrada;
 
 import javax.swing.SwingUtilities;
 
