ab2f7b57f5b775e8ebd2a360bff6474e798b1cd5
Adri <adrian.olmo@cesjuanpablosegundo.es>
2019-10-25 14:37:25 +0200

    .

---
+++ T2 - ElementosGraficos/Botones/src/paneles/PestaniaTres.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paneles;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PestaniaTres extends JPanel implements ActionListener {
+
+    JButton botonNormal, botonImagen;
+
+    public PestaniaTres(){
+
+        initGUI();
+    }
+
+    public void initGUI() {
+        instancias();
+        configurarPanel();
+        acciones();
+    }
+
+    private void acciones() {
+        
+    }
+
+    private void configurarPanel() {
+        
+    }
+
+    private void instancias() {
+       
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+       
+    }
+}
