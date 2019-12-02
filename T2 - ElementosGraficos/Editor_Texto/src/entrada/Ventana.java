b9ce81729c578d9578963f1b1ce5a985fe0fd242
Adrian <adrian.olmo@cesjuanpabloseguno.es>
2019-12-02 19:48:21 +0100

    Editor de Texto

--- T2 - ElementosGraficos/Editor_Texto/src/entrada/Ventana.java
+++ T2 - ElementosGraficos/Editor_Texto/src/entrada/Ventana.java
@@ -5,14 +5,28 @@
  */
 package entrada;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.awt.GridLayout;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
 import javax.swing.KeyStroke;
 import javax.swing.WindowConstants;
 
@@ -22,16 +36,28 @@ import javax.swing.WindowConstants;
  */
 public class Ventana extends JFrame {
 
-    //Container container;
+    Container container;
+    JPanel panelSuperior, panelCentro;
+    JTextPane texto;
     JMenuBar menuEditor;
     JMenu menuArchivo, menuEdicion, menuEstilo;
     JMenuItem itemNuevo, itemAbrir, itemCerrar, itemGuardar,
             itemGuardarcomo, itemImprimir, itemCopiar, itemCortar, itemPegar,
             itemBold, itemNormal, itemCursiva;
 
+    JComboBox comboLetra, comboTamanio, comboTipo;
+    DefaultComboBoxModel modeloLetra, modeloTamanio, modeloTipo;
+    JLabel labelLetra, labelTamanio, labelTipo, labelEstilo;
+    JButton btnpegar, btncopiar, btncortar, btnimprimir, btnnuevo, btnabrir, btnguardar;
+
     public void initGUI() {
         instancias();
         configmenu();
+        configVentana();
+        rellenarLetras();
+        rellenartamanio();
+        rellenartipo();
+        cambiarletras();
         this.setSize(new Dimension(700, 400));
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
@@ -40,31 +66,89 @@ public class Ventana extends JFrame {
 
     private void configmenu() {
         menuEditor.add(menuArchivo);
-        
-        menuArchivo.add(itemNuevo);
-        menuArchivo.add(itemAbrir);
+
+        menuArchivo.add(itemNuevo); //*
+        menuArchivo.add(itemAbrir); //*
         menuArchivo.add(itemCerrar);
-        menuArchivo.add(itemGuardar);
-        menuArchivo.add(itemGuardarcomo);
-        menuArchivo.add(itemImprimir);
-        
+        menuArchivo.add(itemGuardar); //*
+        menuArchivo.add(itemGuardarcomo); //no lleva icono
+        menuArchivo.add(itemImprimir); //*
+
         menuEditor.add(menuEdicion);
-        
-        menuEdicion.add(itemCopiar);
-        menuEdicion.add(itemCortar);
-        menuEdicion.add(itemPegar);
-        
+
+        menuEdicion.add(itemCopiar); //*
+        menuEdicion.add(itemCortar); //*
+        menuEdicion.add(itemPegar); //*
+
         menuEdicion.add(menuEstilo);
-        menuEstilo.add(itemBold);
-        menuEstilo.add(itemNormal);
-        menuEstilo.add(itemCursiva);
-        
+        menuEstilo.add(itemBold); //*
+        menuEstilo.add(itemNormal); //*
+        menuEstilo.add(itemCursiva); //*
+
         this.setJMenuBar(menuEditor);
+    }
+
+    private void configVentana() {
+        container.add(configurarPanelSuperior(), BorderLayout.NORTH);
+        container.add(configurarPanelCentro(), BorderLayout.CENTER);
+    }
+
+    private Component configurarPanelSuperior() {
+        this.setLayout(new GridLayout(2, 1));
+        panelSuperior.add(btnnuevo);
+        panelSuperior.add(btnabrir);
+        panelSuperior.add(btnguardar);
+        panelSuperior.add(btnimprimir);
+        panelSuperior.add(btncopiar);
+        panelSuperior.add(btncortar);
+        panelSuperior.add(btnpegar);
+        panelSuperior.add(comboLetra);
+        panelSuperior.add(comboTamanio);
+        panelSuperior.add(comboTipo);
+        return panelSuperior;
+    }
+
+    private Component configurarPanelCentro() {
+        panelCentro.add(texto);
+        return panelCentro;
+    }
+
+    private void cambiarletras() {
+        Font fuente = new Font((String) modeloLetra.getSelectedItem(),
+                comboTipo.getSelectedIndex(),
+                (int) modeloTamanio.getSelectedItem());
+        Component[] components = this.getComponents();
+        for (Component item : components) {
+            item.setFont(fuente);
+        }
+    }
+
+    private void rellenarLetras() {
+        Font[] fuentes = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
+        for (Font item : fuentes) {
+            modeloLetra.addElement(item.getName());
+        }
+    }
+
+    private void rellenartamanio() {
+        for (int i = 8; i <= 30; i++) {
+            modeloTamanio.addElement(i);
+        }
+    }
+
+    private void rellenartipo() {
+        modeloTipo.addElement("NORMAL");
+        modeloTipo.addElement("BOLD");
+        modeloTipo.addElement("ITALIC");
     }
 
     private void instancias() {
 
-        //container = this.getContentPane();
+        container = this.getContentPane();
+        panelCentro = new JPanel();
+        panelSuperior = new JPanel();
+        texto = new JTextPane();
+        //texto.setPreferredSize(new Dimension(700, 400));
         menuEditor = new JMenuBar();
         /*---------------------------------------------------------------------*/
 
@@ -109,6 +193,48 @@ public class Ventana extends JFrame {
         itemBold = new JMenuItem("Negrita");
         itemNormal = new JMenuItem("Normal");
         itemCursiva = new JMenuItem("Cursiva");
+
+        /*---------------------------------------------------------------------*/
+        modeloLetra = new DefaultComboBoxModel();
+        modeloTipo = new DefaultComboBoxModel();
+        modeloTamanio = new DefaultComboBoxModel();
+
+        comboLetra = new JComboBox(modeloLetra);
+        comboTamanio = new JComboBox(modeloTamanio);
+        comboTipo = new JComboBox(modeloTipo);
+
+        labelTipo = new JLabel("Tipo");
+        labelTamanio = new JLabel("Tamaño");
+        labelLetra = new JLabel("Letra");
+
+        /*---------------------------------------------------------------------*/
+        btnnuevo = new JButton();
+        btnnuevo.setPreferredSize(new Dimension(24, 24));
+        btnnuevo.setIcon(new ImageIcon(getClass().getResource("../resources/new-file.png")));
+
+        btnabrir = new JButton();
+        btnabrir.setPreferredSize(new Dimension(24, 24));
+        btnabrir.setIcon(new ImageIcon(getClass().getResource("../resources/open-file.png")));
+
+        btnguardar = new JButton();
+        btnguardar.setPreferredSize(new Dimension(24, 24));
+        btnguardar.setIcon(new ImageIcon(getClass().getResource("../resources/save.png")));
+
+        btnimprimir = new JButton();
+        btnimprimir.setPreferredSize(new Dimension(24, 24));
+        btnimprimir.setIcon(new ImageIcon(getClass().getResource("../resources/imprimir.png")));
+
+        btncopiar = new JButton();
+        btncopiar.setPreferredSize(new Dimension(24, 24));
+        btncopiar.setIcon(new ImageIcon(getClass().getResource("../resources/copiar.png")));
+
+        btncortar = new JButton();
+        btncortar.setPreferredSize(new Dimension(24, 24));
+        btncortar.setIcon(new ImageIcon(getClass().getResource("../resources/cortar.png")));
+
+        btnpegar = new JButton();
+        btnpegar.setPreferredSize(new Dimension(24, 24));
+        btnpegar.setIcon(new ImageIcon(getClass().getResource("../resources/pegar.png")));
 
     }
 
