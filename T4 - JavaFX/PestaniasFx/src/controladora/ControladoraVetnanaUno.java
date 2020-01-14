4d5bf4ba56f516759891712f096ead619e9d225f
Adri <adrian.olmo@cesjuanpablosegundo.es>
2020-01-14 09:52:09 +0100

    Pestañas JavaFx

--- T4 - JavaFX/PestaniasFx/src/controladora/ControladoraVetnanaUno.java
+++ T4 - JavaFX/PestaniasFx/src/controladora/ControladoraVetnanaUno.java
@@ -6,15 +6,20 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import utils.Persona;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -89,6 +94,23 @@ public class ControladoraVetnanaUno implements Initializable {
                         System.out.println(seleccionado.getEstado());
                     }
                 });
+            }
+        });
+
+        btnSiguiente.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Stage ventanaAdicional =  new Stage();
+                Parent root = null;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("../layout/LayoutVentanaDos.fxml"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Scene scene = new Scene(root, 300, 300);
+                ventanaAdicional.setScene(scene);
+                ventanaAdicional.setTitle("Adicional");
+                ventanaAdicional.show();
             }
         });
 
