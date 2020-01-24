a4703125cc75903d79868dc32ebbe9dd65c88f7b
Adri <adrian.olmo@cesjuanpablosegundo.es>
2020-01-24 14:42:07 +0100

    Pedidos pizzeria

--- T4 - JavaFX/Pizzeria/src/ventanas/Ventana.java
+++ T4 - JavaFX/Pizzeria/src/ventanas/Ventana.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Ventana  extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
