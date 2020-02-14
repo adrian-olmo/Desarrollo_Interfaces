5a076024f8afe5bb9da54349eeb9134583aa52f2
Adri <adrian.olmo@cesjuanpablosegundo.es>
2020-02-14 14:08:25 +0100

    Inicio Angular

---
+++ T5 - Angular/Inicio Angular/inicio/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from "@angular/core";
+import { Tecnologia } from './utils/tecnologia';
+import { TecnologiaInt } from "./utils/tecnologia_dos";
+import { Asignatura } from './utils/asignatura';
+import { DatosCiclosService } from './services/datos-ciclos.service';
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+
+  title = "Introduccion Angular";
+  nombre: string = "Adrian Olmo";
+  mostrarTecnologias: boolean = true;
+  mostrarCurso: string;
+  tecnologias: Tecnologia[];
+  asignaturas: Asignatura[];
+
+  constructor(private servicioDatos: DatosCiclosService){
+  }
+
+  ngOnInit(): void {
+    this.asignaturas = this.servicioDatos.getAllAsignaturas();
+    this.tecnologias = this.servicioDatos.getAllTecnologias();
+  }
+
+
+  mandarMensaje(elemento: Tecnologia) {
+    console.log(elemento.getDescripcion());
+  }
+
+  cambiarTecnologia() {
+    console.log(this.mostrarTecnologias);
+    this.mostrarTecnologias = !this.mostrarTecnologias;
+  }
+
+  cambiarCurso(elemento: string) {
+    this.mostrarCurso = elemento;
+  }
+}
--- T5 - Angular/Inicio Angular/introduccion/src/app/utils/asignatura.ts
+++
@@ -1,9 +0,0 @@
-import { Tecnologia } from "./tecnologia";
-
-export interface Asignatura{
-    nombre:string
-    ciclo:string
-    siglas:string
-    tecnologias:Tecnologia[] 
-    conocimientos:String[] 
-}
