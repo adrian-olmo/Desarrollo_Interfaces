048f095cae4730f2808fa774c7df16057d702bcb
Adri <adrian.olmo@cesjuanpablosegundo.es>
2020-02-14 14:09:19 +0100

    Gestion de Componentes

--- T5 - Angular/Inicio Angular/GestionComponentes/src/app/app.module.ts
+++ T5 - Angular/Inicio Angular/GestionComponentes/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraSuperiorComponent } from './components/barra-superior/barra-superior.component';
+import { CentralComponent } from './components/central/central.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraSuperiorComponent,
+    CentralComponent
   ],
   imports: [
     BrowserModule,
