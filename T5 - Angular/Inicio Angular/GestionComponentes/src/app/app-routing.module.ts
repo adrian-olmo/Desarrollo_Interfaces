4af8bd78a1e2c3869a8ed07439227a2ea6d6eab0
Adri <adrian.olmo@cesjuanpablosegundo.es>
2020-02-18 10:01:48 +0100

    Gestion de Componentes Angular

--- T5 - Angular/Inicio Angular/GestionComponentes/src/app/app-routing.module.ts
+++ T5 - Angular/Inicio Angular/GestionComponentes/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CentralComponent } from './components/central/central.component';
+import { AsignaturasComponent } from './components/asignaturas/asignaturas.component';
 
 
 const routes: Routes = [
-
-  {path: 'central', component: CentralComponent}
+  {path: 'tecnologias', component: CentralComponent},
+  {path: 'tecnologias/:parametro', component: CentralComponent},
+  {path: 'asignaturas/:parametro', component: AsignaturasComponent}
 
 ];
 
