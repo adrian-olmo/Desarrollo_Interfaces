361f1a94f6556ff5ce0e1165b5e52f40bab53721
Adri <adrian.olmo@cesjuanpablosegundo.es>
2020-02-21 13:18:05 +0100

    Gestion Componentes Angular

---
+++ T5 - Angular/Inicio Angular/GestionComponentes/src/app/components/tecnologia-hijo/tecnologia-hijo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tecnologia-hijo',
+  templateUrl: './tecnologia-hijo.component.html',
+  styleUrls: ['./tecnologia-hijo.component.css']
+})
+export class TecnologiaHijoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
