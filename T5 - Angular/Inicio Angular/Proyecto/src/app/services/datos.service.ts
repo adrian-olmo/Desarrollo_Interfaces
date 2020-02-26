74145a40871f9ab4b60bd21f5a4bbd264a622f94
Adri <adrian.olmo@cesjuanpablosegundo.es>
2020-02-26 10:28:59 +0100

    Proyecto Angular

--- T5 - Angular/Inicio Angular/Proyecto/src/app/services/datos.service.ts
+++ T5 - Angular/Inicio Angular/Proyecto/src/app/services/datos.service.ts
@@ -8,23 +8,23 @@ import { element } from 'protractor';
 })
 export class DatosService {
 
-  objJava:Tecnologia = {titulo:"Java", descripcion:"JavaFx, JavaSwing", imagen:"../assets/images/java.png"}
-  objPowerShell:Tecnologia = {titulo:"PowerShell", descripcion:"PowerShell", imagen:"../assets/images/powershell.png"}
-  objAngular:Tecnologia = {titulo:"Angular", descripcion:"Angular", imagen:"../assets/images/anguar.jpeg"}
-  objJS:Tecnologia = {titulo:"JS", descripcion:"JavaScript", imagen:"../assets/images/js.jpg"}
-  objHTML:Tecnologia = {titulo:"HTML", descripcion:"HTML, CSS", imagen:"../assets/images/html.jpeg"}
-  objXML:Tecnologia = {titulo:"XML", descripcion:"XML", imagen:"../assets/images/xml.png"}
-  objSQL:Tecnologia = {titulo:"SQL", descripcion:"SQL", imagen:"../assets/images/mysql.jpg"}
-  objPython:Tecnologia = {titulo:"Python", descripcion:"Python", imagen:"../assets/images/python.jpg"}
+  objJava:Tecnologia = {titulo:"Java", descripcion:"Lenguaje de programación multiplataforma dedicado tanto a la parte de servido como a la parte de cliente", imagen:"../assets/images/java.png"}
+  objPowerShell:Tecnologia = {titulo:"PowerShell", descripcion:"Lenguaje de programación destinado a la automaticación de tareas y la administración de sistemas operativos", imagen:"../assets/images/powershell.png"}
+  objAngular:Tecnologia = {titulo:"Angular", descripcion:"Framework de programación multiplataforma dedicado sobre todo a la parte del FrontEnd", imagen:"../assets/images/anguar.jpeg"}
+  objJS:Tecnologia = {titulo:"JS", descripcion:"Lenguaje de programación destinado a la automaticación de tareas y la administración de sistemas operativos", imagen:"../assets/images/js.jpg"}
+  objHTML:Tecnologia = {titulo:"HTML/CSS", descripcion:"Lenguaje de programación destinado a la automaticación de tareas y la administración de sistemas operativos", imagen:"../assets/images/html.jpeg"}
+  objXML:Tecnologia = {titulo:"XML", descripcion:"XML", imagen:"../assets/images/xml.jpg"}
+  objSQL:Tecnologia = {titulo:"SQL", descripcion:"Lenguaje de programación destinado a la automaticación de tareas y la administración de sistemas operativos", imagen:"../assets/images/mysql.jpg"}
+  objPython:Tecnologia = {titulo:"Python", descripcion:"Lenguaje de programación destinado a la automaticación de tareas y la administración de sistemas operativos", imagen:"../assets/images/python.jpg"}
   tecnologias:Tecnologia[] = [this.objJava, this.objPowerShell, this.objAngular,this.objJS, this.objHTML, this.objXML, this.objSQL, this.objPython]
 
   asignaturasDAM: Asignatura[] = [
     {nombre: "Programación",ciclo: "DAM1",siglas: "PRO",tecnologias: [this.objJava],conocimientos: ["Programación", "Herencia", 'Polimorfismo']}, 
     {nombre: "Sistemas",ciclo: "DAM1",siglas: "SI",tecnologias: [this.objJS],conocimientos: ['Sistemas Operativos', 'Windows', 'Linux']}, 
-    {nombre: "Lenguaje de marcas",ciclo: "DAM1",siglas: "XML",tecnologias: [this.objJS, this.objHTML],conocimientos: ['XML', 'Webs', 'Wordpress']},
-    {nombre: "Desarrollo de interfaces",ciclo: "DAM2",siglas: "DI",tecnologias: [this.objJava, this.objAngular],conocimientos: ["Utilizadad", "Interfaces", "Multiplataforma"]}, 
-    {nombre: "Programación multimedia",ciclo: "DAM2",siglas: "PMDM",tecnologias: [this.objJava, this.objAngular],conocimientos: ['Android', 'Moviles', 'XML']}, 
-    {nombre: "Programación de proceses",ciclo: "DAM2",siglas: "PSP",tecnologias: [this.objJava, this.objAngular],conocimientos: ['Hilos', 'Procesos', 'Comunicaciones']}
+    {nombre: "Lenguaje de marcas",ciclo: "DAM1",siglas: "LM",tecnologias: [this.objJS, this.objHTML],conocimientos: ['XML', 'Webs', 'Wordpress']},
+    {nombre: "Desarrollo de interfaces",ciclo: "DAM2",siglas: "DI",tecnologias: [this.objJava, this.objAngular],conocimientos: ["Utilizadad ", "Interfaces", " Multiplataforma"]}, 
+    {nombre: "Programación multimedia",ciclo: "DAM2",siglas: "PMDM",tecnologias: [this.objJava, this.objAngular],conocimientos: ['Android', ' Moviles', ' XML']}, 
+    {nombre: "Programación de procesos",ciclo: "DAM2",siglas: "PSP",tecnologias: [this.objJava, this.objAngular],conocimientos: ['Hilos', ' Procesos', ' Comunicaciones']}
   ];
 
   constructor() { }
